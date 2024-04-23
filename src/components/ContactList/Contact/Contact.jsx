8a544646d76130eaa93c87c9b36a5f5eb491a5fe
SobDim <101928077+SobDim@users.noreply.github.com>
2024-04-24 01:11:48 +0300

    add modal delete

--- src/components/ContactList/Contact/Contact.jsx
+++ src/components/ContactList/Contact/Contact.jsx
@@ -3,6 +3,22 @@ import { IoPerson } from 'react-icons/io5';
 import s from './Contact.module.css';
 import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../../redux/contacts/operations';
+import { useState } from 'react';
+import { Box, Button, Modal } from '@mui/material';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 const Contact = ({ contact }) => {
   const { name, number, id } = contact;
@@ -10,6 +26,14 @@ const Contact = ({ contact }) => {
   const handleDelete = () => {
     dispatch(deleteContact(id));
   };
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <li className={s.li_wrapper}>
@@ -25,10 +49,16 @@ const Contact = ({ contact }) => {
             </a>
           </p>
         </div>
-
-        <button type="button" onClick={() => handleDelete(id)}>
-          Delete
-        </button>
+        <Button onClick={handleOpen}>Delete</Button>
+        <Modal open={open} onClose={handleClose}>
+          <Box sx={{ ...style, width: 400 }}>
+            <h2 id="parent-modal-title">Delete</h2>
+            <p id="parent-modal-description">{name}</p>
+            <button type="button" onClick={() => handleDelete(id)}>
+              Delete
+            </button>
+          </Box>
+        </Modal>
       </li>
     </>
   );
